02379a8e5deccd03dba0161f7434585c084d2946
ohtake125 <chocolate125late@gmail.com>
2024-07-27 16:04:42 +0900

    image.py修正

--- project/image.py
+++ project/image.py
@@ -1,31 +1,29 @@
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input, decode_predictions
 import numpy as np
 
-# モデルのロード 
+# モデルの読み込み
 model = tf.keras.models.load_model('image_model.h5')
 
-# テスト画像のパス
-img_path = 'project/static/images/'
+# クラスラベル（モデル訓練時に使用したものと一致させる必要があります）
+class_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise']  # 例: 7つのクラス
 
 # 画像の読み込みと前処理
-img = image.load_img(img_path, target_size=(48, 48), color_mode='grayscale') # Change target_size to (48, 48) and convert to grayscale
-x = image.img_to_array(img)
-x = np.expand_dims(x, axis=0)
-#x = preprocess_input(x) # Remove this line as VGG16 preprocessing is not needed
+def preprocess_image(img_path, target_size=(48, 48)):
+    img = image.load_img(img_path, target_size=target_size, color_mode='grayscale')
+    img_array = image.img_to_array(img)
+    img_array = img_array / 255.0  # 正規化
+    img_array = np.expand_dims(img_array, axis=0)  # バッチサイズの次元を追加
+    return img_array
 
-# 予測の実行
-preds = model.predict(x)
+# 画像のパス
+img_path = 'project/static/images/'
+img_array = preprocess_image(img_path)
 
-# 結果の表示 - decode_predictions is not applicable for this model
-print('Predicted:', preds)
+# 予測
+predictions = model.predict(img_array)
+predicted_class_index = np.argmax(predictions, axis=1)
 
-# Get the predicted class index
-predicted_class_index = np.argmax(preds)
-
-# Assuming you have a list of class names
-class_names = ['Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise']  # Replace with your actual class names
-
-# Print the predicted class
-print('Predicted class:', class_names[predicted_class_index])
+# クラスラベルの表示
+predicted_label = class_labels[predicted_class_index[0]]
+print(f'予測された感情ラベル: {predicted_label}')
